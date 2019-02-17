be4d44c34ce7a9a5da04538e24dacae30275f9c1
Exilesprx <campbell.andrew86@yahoo.com>
2019-02-17 02:13:08 -0500

    Query by company name

--- graphql/schema.js
+++ graphql/schema.js
@@ -1,5 +1,5 @@
 const { buildSchema } = require('graphql');
-const { GraphQLSchema, GraphQLObjectType, GraphQLString} = require('graphql');
+const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLList} = require('graphql');
 const gadgetGraphQLType =  require('./gadgetType');
 const Gadget = require('../models/gadget');
 
@@ -10,11 +10,18 @@ const RootQuery = new GraphQLObjectType({
         type: gadgetGraphQLType,
         args: { id: { type: GraphQLString }},
         resolve(parent, args) {
-          return Gadget.findById(args.id)
+          return Gadget.findById(args.id);
+        }
+      },
+      byCompany: {
+        type: new GraphQLList(gadgetGraphQLType),
+        args: { name: {type: GraphQLString}},
+        resolve(parent, args) {
+          return Gadget.find({ by_company: args.name });
         }
       }
     }
-  })
+  });
 
   module.exports = new GraphQLSchema({
     query: RootQuery
